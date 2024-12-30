95617ff8ed9f382298f60dadda79fd0479a78850
DvorakFilip <filidvor18@gmail.com>
2024-12-30 20:02:43 +0100

    Now returns list of areas (of chosen color)

--- functions.py
+++ functions.py
@@ -1,8 +1,5 @@
 
 
-def prettyprint(my_2d_list, width):
-    for i in range(len(my_2d_list)):
-        if (i != 0) and (i % width == 0):
-            print()
-        print(my_2d_list[i], end="")
-    print()
+def prettyprint(my_2d_list):
+    for i in my_2d_list:
+        print(i)
--- main.py
+++ main.py
@@ -2,22 +2,79 @@ from PIL import Image
 
 from functions import prettyprint
 
+# ask for path to image and searched color
 image_path = input("Enter image path: ")
-important_color = input("Enter RGB value for color: ")
-print()
-image_path = "testinput/black03.png"
-
 if image_path == "quit":
     quit()
+color = input("Enter RGB value for color (format: '255 255 255'): ")
+color = tuple([int(i) for i in color.split(" ")])
+print(color)
+print()
+
+#temporary
+image_path = "testinput/black03.png"
 
 
 
+# Convert the image to list of RGB values
 with Image.open(image_path) as img:
 
     img = img.convert("RGB")
     width, height = img.size
 
-    rgb_values = list(img.getdata())
+    all_rgb_values = list(img.getdata())
 
-#print(width, height)
-#prettyprint(rgb_values, width)
+
+# converting list of RGB values to 2d matrix
+rgb_values = []
+for i in range(height):
+    rgb_values.append(all_rgb_values[width*i:width*(i+1)])
+
+
+
+def bfs(x, y):
+    global rgb_values
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    visited = []
+    queue = [(x, y)]
+
+    while queue:
+
+        x, y = queue.pop(0)
+
+        if rgb_values[y][x] != color:
+            continue
+
+        visited.append((x, y))
+
+        for direction in directions:
+            new_x, new_y = x+direction[0], y+direction[1]
+            if ((new_x, new_y) not in visited) and ((new_x >= 0) and (new_x < width)) and ((new_y >= 0) and (new_y < height)):
+                queue.append((new_x, new_y))
+
+    return visited
+
+
+
+
+
+
+result = []
+visited = []
+for y, row in enumerate(rgb_values):
+    for x, value in enumerate(row):
+
+        if (value == color) and ((x, y) not in visited):
+            new_pixels = bfs(x, y)
+            visited += new_pixels
+            #print(new_pixels)
+            result.append(new_pixels)
+
+
+
+
+prettyprint(rgb_values)
+#print()
+print(len(result), result)
+
