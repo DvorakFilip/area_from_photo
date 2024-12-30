8a41a59433858224d60c869054b623d3aa9d83cb
DvorakFilip <filidvor18@gmail.com>
2024-12-30 20:24:48 +0100

    Fixing some bugs in bfs.

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ image_path = input("Enter image path: ")
 if image_path == "quit":
     quit()
 color = input("Enter RGB value for color (format: '255 255 255'): ")
-color = tuple([int(i) for i in color.split(" ")])
-print(color)
-print()
+if color != "":
+    color = tuple([int(i) for i in color.split(" ")])
 
 #temporary
-image_path = "testinput/black03.png"
+image_path = "testinput/black04.png"
+color = (0, 0, 0)
 
 
 
@@ -36,24 +36,26 @@ def bfs(x, y):
     global rgb_values
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-    visited = []
+    visited = [(x, y)]
+    result = []
     queue = [(x, y)]
 
     while queue:
 
         x, y = queue.pop(0)
 
-        if rgb_values[y][x] != color:
-            continue
+        if rgb_values[y][x] == color:
+            result.append((x, y))
 
-        visited.append((x, y))
 
-        for direction in directions:
-            new_x, new_y = x+direction[0], y+direction[1]
-            if ((new_x, new_y) not in visited) and ((new_x >= 0) and (new_x < width)) and ((new_y >= 0) and (new_y < height)):
-                queue.append((new_x, new_y))
 
-    return visited
+            for direction in directions:
+                new_x, new_y = x+direction[0], y+direction[1]
+                if ((new_x, new_y) not in visited) and ((new_x >= 0) and (new_x < width)) and ((new_y >= 0) and (new_y < height)):
+                    queue.append((new_x, new_y))
+                    visited.append((new_x, new_y))
+
+    return result
 
 
 
@@ -75,6 +77,12 @@ for y, row in enumerate(rgb_values):
 
 
 prettyprint(rgb_values)
-#print()
-print(len(result), result)
+print()
+
+
+print("Number of areas: ", len(result))
+for i, r in enumerate(result):
+    print("Area number: ", i)
+    print(len(r), " pixels:")
+    print(r)
 
