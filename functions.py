84d6dd1c7dad8a08aee81c41c9bb39023c2a56a7
DvorakFilip <filidvor18@gmail.com>
2025-01-10 22:10:59 +0100

    finalizing

--- functions.py
+++
@@ -1,5 +0,0 @@
-
-
-def prettyprint(my_2d_list):
-    for i in my_2d_list:
-        print(i)
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from PIL import Image
 import numpy as np
-from imagedemonstration import save_processed_image
 
-from functions import prettyprint
 
 # ask for path to image, searched color and tolerance
 image_path, color, tolerance = "1", "1", "1"
@@ -22,7 +20,7 @@ tolerance = int(tolerance)
 
 
 #temporary
-image_path = "testinput/black02.png"
+image_path = "testinput/black03.png"
 color = (0, 0, 0)
 tolerance = 20
 
@@ -30,14 +28,12 @@ tolerance = 20
 
 # Convert the image to list of RGB values
 img = Image.open(image_path)
-img_processed = Image.open(image_path)
-pixels_processed = img_processed.load()
 
 img_rgb = img.convert("RGB")
 width, height = img_rgb.size
 
 all_rgb_values = list(img_rgb.getdata())
-
+pixels_processed = img.load()
 
 # converting list of RGB values to 2d matrix
 rgb_values = []
@@ -61,20 +57,19 @@ def bfs(x, y):
 
         x, y = queue.pop(0)
 
-
-        if len(result)%1000 == 0:
-            print(len(result))
-
         for direction in directions:
+
             new_x, new_y = x+direction[0], y+direction[1]
+
             if ((new_x, new_y) not in visited) and ((new_x >= 0) and (new_x < width)) and ((new_y >= 0) and (new_y < height)):
+
                 visited.add((new_x, new_y))
                 rgb_value = rgb_values[new_y][new_x]
+
                 if (abs(color[0]-rgb_value[0]) <= tolerance) and (abs(color[1]-rgb_value[1]) <= tolerance) and (abs(color[2]-rgb_value[2]) <= tolerance):
                     queue.append((new_x, new_y))
                     result.append((new_x, new_y))
     
-    #print("")
 
     return result
 
@@ -88,26 +83,25 @@ visited = set()
 for y, row in enumerate(rgb_values):
 
     print(f"{(y/height)*100:.2f} %")
+
     for x, value in enumerate(row):
 
         if ((x, y) not in visited):
+
             if (abs(color[0]-value[0]) <= tolerance) and (abs(color[1]-value[1]) <= tolerance) and (abs(color[2]-value[2]) <= tolerance):
                 new_pixels = bfs(x, y)
                 visited.update(new_pixels)
-                print(f"lenght of result: {len(new_pixels)}")
-                print(new_pixels)
-                print()
                 result.append(new_pixels)
+
             else:
                 pixels_processed[x,y] = (100, 100, 100)
 
 
 
 
-#prettyprint(rgb_values)
-print()
 
 
+# FINAL OUTPUT AND COMPUTING THE AREA
 print("Number of areas: ", len(result))
 
 surface_areas = []
@@ -116,7 +110,6 @@ rs = []
 for i, r in enumerate(result):
     print("Area number: ", i)
     print(len(r), " pixels")
-    #print(r)
 
     # since len(r) is number of pixels of color area, it coresponds to area of a circle
     r = np.sqrt((len(r)/np.pi))
@@ -130,9 +123,8 @@ if len(rs) == 0:
 print("Average diameter: ", (sum(rs)/len(rs)))
 print("Total surface area: ", (sum(surface_areas)))
 
-#bonus - visualize selection
+#Creates image mask for chosen color
 output = f"{image_path.split(".")[0]}_processed2.png" 
 print((output))
-img_processed.save(output)
-save_processed_image(img, color, tolerance).save((f"{image_path.split(".")[0]}_processed.png"))
+img.save(output)
 
